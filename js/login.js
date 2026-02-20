ad59be774fc7266bfaaf9370f435ae30b07e9f37
mehedi-hasan2006 <mehedihasan811914@gmail.com>
2026-02-20 19:29:30 +0600

    login problem solved

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ document.getElementById('login-btn').addEventListener('click', function(){
 
     if(loginNumber === '01234567890' && loginPin === '1234'){
         alert("Login Success");
-        window.location.assign('https://mehedi-hasan2006.github.io/payoo-mobile-banking-app/home.html');
+        window.location.assign('/home.html');
     }
     else{
         alert('Login Failed');
